221db2105612467d962acf4c8f528caa360418b8
Pedro Joaquim <pedro.joaquim.g01@gmail.com>
2022-05-22 12:19:19 -0300

    Código de importação de csv para database

---
+++ importar_csv_para_sql.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import sqlite3
+import pandas as pd
+
+Path('my_data.db').touch()
+
+conn = sqlite3.connect('my_data.db')
+c = conn.cursor()
+
+c.execute('''CREATE TABLE produtos_farmaceuticos (CodigoNFe int, DataEmissao date, MunicipioEmitente text, unidadecomercial text, quantidadecomercial real, valorunitariocomercial real, DescricaoProduto text, CLEAN text)''')
+
+produtos_farmaceuticos = pd.read_csv('produtos_farmaceuticos.csv')
+
+produtos_farmaceuticos.to_sql('produtos_farmaceuticos', conn, if_exists='append', index = False)
+
+c.execute('''SELECT * FROM produtos_farmaceuticos''').fetchall()
